b85620c458dd13f35b9aba6866b5cf0dcb1651cc
ChobitsKS <chobits.ks@gmail.com>
2026-01-12 15:23:18 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
+// GLOBAL LOGGER: Log every request!
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
+
 // Main Route
 app.get('/', (req, res) => {
     res.send('VERSION 2: READY - Server is Online! 🚀');
